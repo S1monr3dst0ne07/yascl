84e512f5fe57c2a193a1e2d4204185a270bda473
s1monr3dst0ne07 <s1monr3dst0ne07@outlook.com>
2026-07-27 03:37:30 +0200

    wrote bash script for verification

---
+++ verify.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/bash
+
+function compile 
+{
+    $1 $2
+    mv build.asm $3
+    fasm $3 $4 -m 1000000
+    chmod +x $4
+}
+
+# stage 0, bootstrap compiler
+compile "./bootstrap.py" "src/main.yap" "stage_zero.asm" "stage_zero"
+
+# stage 1, compile compiler with itself
+compile "./stage_zero" "src/main.yap" "stage_one.asm" "stage_one"
+
+# stage 2, compile-compile compiler with itself
+compile "./stage_one" "src/main.yap" "stage_two.asm" "stage_two"
+
+# now stage_one and stage_two should be identical
+if diff stage_one stage_two; then
+    echo "compiler valid!"
+else
+    echo "compiler invalid!"
+fi
+
+
+rm stage_zero*
+rm stage_one*
+rm stage_two*
+
+
